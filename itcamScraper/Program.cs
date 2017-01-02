61518c1688da5e6a4812c81c7161b65897646c68
mayur <mayur@DESKTOP-9O42QRJ>
2017-01-02 15:53:20 -0800

    Checks Network Drive and iterates through missing dates.  Also ExcelMacro take path and date as arguments

--- itcamScraper/Program.cs
+++ itcamScraper/Program.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Diagnostics;
-
+using System.Collections;
+using System.Threading;
 
 namespace itcamScraper
 {
@@ -13,23 +14,83 @@ namespace itcamScraper
         
         static void Main(string[] args)
         {
-            string batFile = "itcamscraper.bat";
-           // createBatFile(batFile);
-           // runBatFile(batFile);
-
+            
+            //Copies Java Jar files and spreadsheets to My Documents folder
+            copyFileFolder();
 
             //Check last files uploaded last 30 days
-            copyFileFolder();
-            createBatFile(batFile);
-            runBatFile(batFile);
-            //run AutoIt script to open and run VB Script in Excel file
-            runAutoItScript();
+            ArrayList dates = checkTargetFolder();
+            Console.WriteLine("ITCAM Scraper will process " + dates.Count.ToString() + " days worth of data");
+            string itCamPass = new StreamReader(Environment.CurrentDirectory + "\\itCamPassword.txt").ReadLine();
+            for (int i=0; i<dates.Count; i++)
+            {
+                Console.WriteLine("Processing ITCAM data for " + dates[i].ToString());
+                //Thread.Sleep(5000);
+                string javaBatFile = "itcamscraper.bat";
+                string javaCommand = "java -cp DailyGathering.jar;jsoup-1.8.3.jar gov.ca.dmv.ea.perf.ItcamWSI2 ItcamWSI2.props " + dates[i].ToString() + " \"" + itCamPass + "\"";
+                //run Java Application to download csv files for 1 specific date
+                createBatFile(javaBatFile, dates[i].ToString(), javaCommand);
+                runBatFile(javaBatFile);
+                //run AutoIt script to open and run VB Script in Excel file
+                runAutoItScript(dates[i].ToString());
+            }
 
         }
 
-        private static void runAutoItScript()
+        private static ArrayList checkTargetFolder()
         {
-          
+            ArrayList dates = new ArrayList();
+            ArrayList scrapeDates = new ArrayList();
+            String scrapeDate = DateTime.Now.AddDays(-1).ToString("MM dd yyyy");
+            for(int i=1; i<30; i++)
+            {
+                dates.Add(DateTime.Now.AddDays(-i).ToString("MM dd yyyy"));
+            }
+            for(int j=0; j<dates.Count; j++)
+            {
+                if(folderExists(dates[j].ToString()))
+                {
+                    scrapeDates.Add(dates[j].ToString());
+                }
+            }
+            return dates;
+        }
+
+        private static bool folderExists(string date)
+        {
+            string networkFolder = new StreamReader(Environment.CurrentDirectory + "\\networkTargetFolderPath.txt").ReadLine();
+            DateTime pdate = DateTime.Parse(date);
+            string[] dateStrings = pdate.ToString("MMM dd yyyy").Split(' ');
+            string month = dateStrings[0];
+            string day = dateStrings[1];
+            if(day.StartsWith("0"))
+            {
+                day = day.TrimStart('0');
+            }
+            string year = dateStrings[2];
+
+            string path = networkFolder + "\\" + year + "\\" + month + "\\" + month + "_" + day + "_" + year ;
+            return Directory.Exists(path);
+            
+        }
+
+        private static void runAutoItScript(string scrapeDate)
+        {
+            string batFile = "autoItExcel.bat";
+            string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\myDocsTargetFolderPath.txt").ReadLine();
+            DateTime pdate = DateTime.Parse(scrapeDate);
+            string[] dateStrings = pdate.ToString("MMM dd yyyy").Split(' ');
+            string month = dateStrings[0];
+            string day = dateStrings[1];
+            if (day.StartsWith("0"))
+            {
+                day = day.TrimStart('0');
+            }
+            string year = dateStrings[2];
+            string command = "\"" + myDocFolder + "\\ExcelMacro.exe" + "\" " + "\"" + month + "_" + day + "_" + year + "\"";
+            //run Java Application to download csv files for 1 specific date
+            createBatFile(batFile, scrapeDate, command);
+            runBatFile(batFile);
         }
 
         private static void copyFileFolder()
@@ -37,11 +98,18 @@ namespace itcamScraper
             //Get path from text file
             try
             {
-                string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\targetFolderPath.txt").ReadLine();
-               
+                string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\myDocsTargetFolderPath.txt").ReadLine();
+                
                 string sourceFolder = Environment.CurrentDirectory + "\\source";
-                CopyFolder(sourceFolder, myDocFolder);
-                Console.WriteLine(sourceFolder + " copied successfully to " + myDocFolder);
+                if (Directory.Exists(myDocFolder + "\\" + "WSI2_PROD_PERF"))
+                {
+                    Console.WriteLine(myDocFolder + "\\" + "WSI2_PROD_PERF " + "    Folder already exists");
+                }
+                else
+                {
+                    CopyFolder(sourceFolder, myDocFolder);
+                    Console.WriteLine(sourceFolder + " copied successfully to " + myDocFolder);
+                }
             }
             catch (Exception ex)
             {
@@ -54,13 +122,13 @@ namespace itcamScraper
             Process proc = null;
             try
             {
-                string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\targetFolderPath.txt").ReadLine();
+                string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\myDocsTargetFolderPath.txt").ReadLine();
                 proc = new Process();
                 proc.StartInfo.WorkingDirectory = myDocFolder + "WSI2_PROD_PERF\\";
-                Console.WriteLine("proc.StartInfo.WorkingDirectory: " + proc.StartInfo.WorkingDirectory + " executed");
                 proc.StartInfo.FileName = batFile;
                 proc.StartInfo.CreateNoWindow = false;
                 proc.Start();
+                Console.WriteLine("proc.StartInfo.WorkingDirectory: " + proc.StartInfo.WorkingDirectory + " executed");
                 proc.WaitForExit();
                 Console.WriteLine( batFile + " executed");
             }
@@ -89,15 +157,13 @@ namespace itcamScraper
             }
         }
 
-        private static void createBatFile(string batFile)
+        private static void createBatFile(string batFile, string scrapeDate, string command)
         {
             try
             {
-                string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\targetFolderPath.txt").ReadLine();
-                StreamWriter sw = new StreamWriter(myDocFolder + "\\WSI2_PROD_PERF\\" + batFile);
-                String scrapeDate = DateTime.Now.AddDays(-1).ToString("MM dd yyyy");
-                String javaCommand = "java -cp DailyGathering.jar;jsoup-1.8.3.jar gov.ca.dmv.ea.perf.ItcamWSI2 ItcamWSI2.props " + scrapeDate + " \"mwmz366\"";
-                sw.WriteLine(javaCommand);
+                string myDocFolder = new StreamReader(Environment.CurrentDirectory + "\\myDocsTargetFolderPath.txt").ReadLine();
+                StreamWriter sw = new StreamWriter(myDocFolder + "\\WSI2_PROD_PERF\\" + batFile);  
+                sw.WriteLine(command);
                 sw.Close();
             }
             catch (Exception ex)
